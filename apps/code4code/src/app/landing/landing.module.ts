1f331409ba4dc24d6d3a4d294329a44c9f76235e
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2021-07-18 19:36:29 +0300

    landing topbar and housekeeping

--- apps/code4code/src/app/landing/landing.module.ts
+++ apps/code4code/src/app/landing/landing.module.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 
-import { LandingComponent } from './landing.component';
+import { LandingComponent } from './landing/landing.component';
+import { LandingTopbarComponent } from './landing-topbar/landing-topbar.component';
 
 export const landingRoutes: Route[] = [
   { path: '', component: LandingComponent },
-  // { path: '', component: LandingTopbarComponent, outlet: 'topbar' },
+  { path: '', component: LandingTopbarComponent, outlet: 'topbar' },
   // { path: 'signup', component: SignupComponent },
 ];
 
 @NgModule({
-  declarations: [LandingComponent],
+  declarations: [LandingComponent, LandingTopbarComponent],
   imports: [CommonModule, RouterModule.forChild(landingRoutes)],
 })
 export class LandingModule {}
