9c740a244d642be8faad40301f273ae36edfb765
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2021-07-18 18:46:32 +0300

    user and landing modules

--- apps/code4code/src/app/app.module.ts
+++ apps/code4code/src/app/app.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        loadChildren: () =>
+          import('./landing/landing.module').then((m) => m.LandingModule),
+      },
+      {
+        path: 'user',
+        loadChildren: () =>
+          import('./user/user.module').then((m) => m.UserModule),
+      },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
