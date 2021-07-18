873f177f393548e0384fbd3c67ac183529ea00b9
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2021-07-18 20:48:05 +0300

    inter fonts and @tailwind/forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,22 @@
-const { guessProductionMode } = require("@ngneat/tailwind");
+const { guessProductionMode } = require('@ngneat/tailwind');
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    prefix: '',
-    purge: {
-      enabled: guessProductionMode(),
-      content: [
-        './apps/**/*.{html,ts}',
-        './libs/**/*.{html,ts}',
-      ]
+  prefix: '',
+  purge: {
+    enabled: guessProductionMode(),
+    content: ['./apps/**/*.{html,ts}', './libs/**/*.{html,ts}'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
     },
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms')],
 };
