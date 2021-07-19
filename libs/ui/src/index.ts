21dc4dd5a1d78b7c85c15737bd3c17506fe99e06
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2021-07-19 17:50:26 +0300

    ui lib barell export

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -3,3 +3,6 @@ export * from './lib/ui.module';
 export * from './lib/interfaces';
 
 export * from './lib/simple-dropdown/simple-dropdown.component';
+export * from './lib/avatar-dropdown/avatar-dropdown.component';
+export * from './lib/dropdown/dropdown.component';
+export * from './lib/avatar-circular/avatar-circular.component';
