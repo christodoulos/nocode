ca1d3b596e1c42c5fa7874f91a485eefa5da7bad
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2021-07-19 10:17:29 +0300

    circular avatar component

---
+++ libs/ui/src/lib/avatar-circular/avatar-circular.component.ts
@@ -0,0 +1,14 @@
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'avatar-circular',
+  templateUrl: './avatar-circular.component.html',
+  styleUrls: ['./avatar-circular.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AvatarCircularComponent {
+  @Input() imageURL$: Observable<string | null | undefined> | undefined;
+  @Input() size = 8;
+}
--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
 import { SimpleDropdownComponent } from './simple-dropdown/simple-dropdown.component';
+import { AvatarCircularComponent } from './avatar-circular/avatar-circular.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-  declarations: [SimpleDropdownComponent],
-  exports: [SimpleDropdownComponent],
+  declarations: [SimpleDropdownComponent, AvatarCircularComponent],
+  exports: [SimpleDropdownComponent, AvatarCircularComponent],
 })
 export class UiModule {}
