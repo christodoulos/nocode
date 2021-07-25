6d16bf3ae26ffd3190e98eea57658a72654b909f
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2021-07-25 13:28:20 +0300

    codemirror 6

---
+++ libs/ui/src/lib/editor/editor.component.ts
@@ -0,0 +1,39 @@
+import {
+  Component,
+  ChangeDetectionStrategy,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
+import { EditorState, EditorView, basicSetup } from '@codemirror/basic-setup';
+import { python } from '@codemirror/lang-python';
+
+@Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'editor',
+  templateUrl: './editor.component.html',
+  styleUrls: ['./editor.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class EditorComponent implements AfterViewInit {
+  @ViewChild('editor') editorElmRef: ElementRef | undefined;
+  editorDiv: HTMLDivElement | undefined;
+  editorView: EditorView | undefined;
+
+  ngAfterViewInit(): void {
+    if (this.editorElmRef) {
+      this.editorDiv = this.editorElmRef.nativeElement;
+      this.editorView = new EditorView({
+        state: EditorState.create({
+          // doc: 'hello',
+          extensions: [basicSetup, python()],
+        }),
+        parent: this.editorDiv,
+      });
+    }
+  }
+
+  onNew() {
+    console.log(this.editorView?.state.doc);
+  }
+}
